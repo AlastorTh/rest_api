c64a4852f3257ba071324814fc9bd02693c6bf97
AlastorTh <alexkasatkin90@gmail.com>
2021-01-10 14:53:37 +0300

    added store & testing

--- internal/app/store/store.go
+++ internal/app/store/store.go
@@ -6,17 +6,21 @@ import (
 	_ "github.com/lib/pq" // ...
 )
 
+// Store ...
 type Store struct {
-	config *Config
-	db     *sql.DB
+	config         *Config
+	db             *sql.DB
+	UserRepository *UserRepository
 }
 
+// New ...
 func New(config *Config) *Store {
 	return &Store{
 		config: config,
 	}
 }
 
+// Open ...
 func (s *Store) Open() error {
 	db, err := sql.Open("postgres", s.config.DatabaseURL)
 	if err != nil {
@@ -31,6 +35,21 @@ func (s *Store) Open() error {
 	return nil
 }
 
+// Close ...
 func (s *Store) Close() {
 	s.db.Close()
 }
+
+// User ...
+func (s *Store) User() *UserRepository {
+	if s.UserRepository != nil {
+		return s.UserRepository
+	}
+
+	s.UserRepository = &UserRepository{
+		store: s,
+	}
+	return s.UserRepository
+}
+
+//store.User().Create()
